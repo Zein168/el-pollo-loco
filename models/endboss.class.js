1e1d7bd2dbb38a6f8cc6299e8da30f3b7898a936
Zein168 <zein.9494@outlook.com>
2026-06-09 18:03:18 +0200

    feat: expand level width

--- models/endboss.class.js
+++ models/endboss.class.js
@@ -19,7 +19,7 @@ class Endboss extends MovableObject {
         super();
         this.loadImage(this.IMAGES_WALKING[0]);
         this.loadImages(this.IMAGES_WALKING);
-        this.x =700;  
+        this.x =5000;  
         this.animate();
     }
 
