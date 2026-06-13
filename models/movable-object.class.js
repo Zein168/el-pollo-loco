fd9ebbec1f90ad5f7ce7b5178066302cef3ddff7
Zein168 <zein.9494@outlook.com>
2026-06-13 14:59:16 +0200

    feat: add death sprite for enemies

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -1,4 +1,4 @@
-class MovableObject extends DrawableObject{
+class MovableObject extends DrawableObject {
     speed = 0.15;
     otherDirection = false;
     speedY = 0;
@@ -8,79 +8,82 @@ class MovableObject extends DrawableObject{
     isDead = false;
 
 
-    applyGravity(){
+    applyGravity() {
         setInterval(() => {
             if (this.isAboveGround() || this.speedY > 0) {
-                this.y -=this.speedY;
+                this.y -= this.speedY;
                 this.speedY -= this.acceleration;
             }
         }, 1000 / 25);
     }
 
-    isAboveGround(){
-        if(this instanceof ThrowableObjects) {
+    isAboveGround() {
+        if (this instanceof ThrowableObjects) {
             return true;
         } else {
             return this.y < 150
         }
-        
+
     }
 
 
 
-    isColliding(mo){
+    isColliding(mo) {
         return this.x + this.width > mo.x &&
-        this.y + this.height > mo.y &&
-        this.x < mo.x &&
-        this.y < mo.y + mo.height
+            this.y + this.height > mo.y &&
+            this.x < mo.x &&
+            this.y < mo.y + mo.height
 
     }
 
-    playAnimation(images){ 
+    playAnimation(images) {
         let i = this.currentImage % images.length;
         let path = images[i];
         this.img = this.imageCache[path];
         this.currentImage++;
     }
 
-    moveRight(){
+    moveRight() {
         console.log("Moving right")
         this.x += this.speed;
-        
+
     }
 
     moveLeft() {
         this.x -= this.speed;
-   
-       
+
+
     }
 
-    jump(){
+    jump() {
         this.speedY = 30;
     }
 
     hit() {
         this.energy -= 5;
-        if(this.energy < 0) {
+        if (this.energy < 0) {
             this.energy = 0;
         } else {
             this.lastHit = new Date().getTime();
         }
     }
 
-    isHurt(){
+    isHurt() {
         let timepassed = new Date().getTime() - this.lastHit;
         timepassed = timepassed / 1000;
         return timepassed < 1.5;
     }
 
-    isDead(){
+    isDead() {
         return this.energy == 0;
     }
 
     die() {
-    this.isDead = true;
-    this.speed = 0;
+        this.isDead = true;
+        this.speed = 0;
+        setTimeout(() => {
+            this.y = 1000;
+        }, 500);
     }
 
 
--- models/small-chicken.class.js
+++ models/small-chicken.class.js
@@ -8,9 +8,14 @@ class SmallChicken extends MovableObject {
         'img/3_enemies_chicken/chicken_small/1_walk/3_w.png',
     ];
 
+    IMAGES_DEAD = [
+    'img/3_enemies_chicken/chicken_small/2_dead/dead.png'
+    ];
+
     constructor(x) {
         super().loadImage('img/3_enemies_chicken/chicken_small/1_walk/1_w.png');
         this.loadImages(this.IMAGES_WALKING);
+        this.loadImages(this.IMAGES_DEAD);
         this.x = x;
         this.y = 370;
         this.speed = 0.15 + Math.random() * 0.5;
@@ -26,7 +31,11 @@ class SmallChicken extends MovableObject {
         }, 1000 / 60);
 
         setInterval(() => {
-            this.playAnimation(this.IMAGES_WALKING)
+            if (this.isDead) {
+                this.playAnimation(this.IMAGES_DEAD);
+            } else {
+                this.playAnimation(this.IMAGES_WALKING);
+            }
         }, 200);
     }
 }
