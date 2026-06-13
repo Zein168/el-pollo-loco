6e1f943b4152436a0551728e686137f0fa3cd21f
Zein168 <zein.9494@outlook.com>
2026-06-13 14:34:19 +0200

    feat: add walking animation to SmallChicken

--- models/small-chicken.class.js
+++ models/small-chicken.class.js
@@ -1,12 +1,29 @@
 class SmallChicken extends MovableObject {
+    y = 370;
+    height = 60;
+    width = 80;
+    IMAGES_WALKING = [
+        'img/3_enemies_chicken/chicken_small/1_walk/1_w.png',
+        'img/3_enemies_chicken/chicken_small/1_walk/2_w.png',
+        'img/3_enemies_chicken/chicken_small/1_walk/3_w.png',
+    ];
 
     constructor(x) {
-        super();
-
-        this.loadImage('img/3_enemies_chicken/chicken_small/1_walk/1_w.png');
+        super().loadImage('img/3_enemies_chicken/chicken_small/1_walk/1_w.png');
+        this.loadImages(this.IMAGES_WALKING);
         this.x = x;
-        this.y = 340;     
-        this.width = 60;
-        this.height = 80;
+        this.y = 370;
+        this.speed = 0.15 + Math.random() * 0.5;
+        this.animate();
+    }
+
+    animate() {
+        setInterval(() => {
+            this.moveLeft();
+        }, 1000 / 60);
+
+        setInterval(() => {
+            this.playAnimation(this.IMAGES_WALKING)
+        }, 200);
     }
 }
