f1ad2a4d1a8da12f81ce1dbeb10db405bd9ade6b
Zein168 <zein.9494@outlook.com>
2026-06-14 13:44:15 +0200

    feat: add random bottle texture selection

--- models/bottles.class.js
+++ models/bottles.class.js
@@ -1,9 +1,15 @@
 class Bottles extends MovableObject {
+    IMAGES = [
+        'img/6_salsa_bottle/1_salsa_bottle_on_ground.png',
+        'img/6_salsa_bottle/2_salsa_bottle_on_ground.png'
+    ];
     constructor(x) {
-        super().loadImage('img/6_salsa_bottle/1_salsa_bottle_on_ground.png');
-
+        super();
+        this.loadImages(this.IMAGES);
+        let randomIndex = Math.floor(Math.random() * this.IMAGES.length);
+        this.loadImage(this.IMAGES[randomIndex]);
         this.x = x;
-        this.y = 340;     
+        this.y = 340;
         this.width = 60;
         this.height = 80;
     }
