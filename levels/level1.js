7abae2cdf3d12f2787eb499426bf44ac4d7bb5be
Zein168 <zein.9494@outlook.com>
2026-06-21 09:44:30 +0200

    fix: add missing background section

--- levels/level1.js
+++ levels/level1.js
@@ -91,6 +91,11 @@ const level1 = new Level(
         new BackgroundObject('img/5_background/layers/2_second_layer/2.png', 719 * 7),
         new BackgroundObject('img/5_background/layers/1_first_layer/2.png', 719 * 7),
 
+        new BackgroundObject('img/5_background/layers/air.png', 719 * 8),
+        new BackgroundObject('img/5_background/layers/3_third_layer/1.png', 719 * 8),
+        new BackgroundObject('img/5_background/layers/2_second_layer/1.png', 719 * 8),
+        new BackgroundObject('img/5_background/layers/1_first_layer/1.png', 719 * 8),
+
 
 
     ],
