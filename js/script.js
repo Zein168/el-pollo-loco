12a095e8117e6ca8182487b97795eae6abb7b843
Zein168 <zein.9494@outlook.com>
2026-06-28 10:16:02 +0200

    feat: add story button with event listener

--- js/script.js
+++ js/script.js
@@ -1,8 +1,5 @@
 window.addEventListener("load", () => {
-
-
     const howToPlay = document.getElementById("howToPlay");
-
     document.getElementById("howToPlayBtn").addEventListener("click", () => {
         howToPlay.classList.remove("hidden");
     });
@@ -11,4 +8,14 @@ window.addEventListener("load", () => {
         howToPlay.classList.add("hidden");
     });
 
+});
+
+window.addEventListener("load", () => {
+    const storyContainer = document.getElementById("storyContainer");
+    document.getElementById("story").addEventListener("click", () => {
+        storyContainer.classList.remove("hidden");
+    });
+    document.getElementById("closeStory").addEventListener("click", () => {
+        storyContainer.classList.add("hidden");
+    });
 });
