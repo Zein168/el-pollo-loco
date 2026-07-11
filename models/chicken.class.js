2323bb0907fde761dd07ae0fc57df863156a6bde
Zein168 <zein.9494@outlook.com>
2026-07-11 15:47:57 +0200

    refactor: rename small-chicken sound variables

--- models/chicken.class.js
+++ models/chicken.class.js
@@ -21,8 +21,8 @@ class Chicken extends MovableObject {
         this.world = world;
         this.speed = 0.15 + Math.random() * 0.5;
         this.isDead = false;
-        this.jumpSound = new Audio('audio/chicken-noise.mp3');
-        this.deathSoundPlayed = false;
+        this.chickenSound  = new Audio('audio/chicken-noise.mp3');
+        this.chickenSoundPlayed = false;
     }
 
     animate() {
@@ -40,9 +40,9 @@ class Chicken extends MovableObject {
         setInterval(() => {
             if (this.isDead) {
                 this.playAnimation(this.IMAGES_DEAD);
-                if (!this.deathSoundPlayed) {
-                    this.jumpSound.play();
-                    this.deathSoundPlayed = true;
+                if (!this.chickenSoundPlayed) {
+                    this.chickenSound.play();
+                    this.chickenSoundPlayed = true;
                 }
             } else {
                 this.playAnimation(this.IMAGES_WALKING);
