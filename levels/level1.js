f79b5e62d9854e0ed66e7adfec3d1026c6663fb4
Zein168 <zein.9494@outlook.com>
2026-07-19 12:37:39 +0200

    fix: restore bottle throw cooldown

--- levels/level1.js
+++ levels/level1.js
@@ -49,7 +49,6 @@ function initLevel1() {
             new BackgroundObject('img/5_background/layers/3_third_layer/2.png', -719,),
             new BackgroundObject('img/5_background/layers/2_second_layer/2.png', -719),
             new BackgroundObject('img/5_background/layers/1_first_layer/2.png', -719),
-
             new BackgroundObject('img/5_background/layers/air.png', 0),
             new BackgroundObject('img/5_background/layers/3_third_layer/1.png', 0),
             new BackgroundObject('img/5_background/layers/2_second_layer/1.png', 0),
@@ -58,7 +57,6 @@ function initLevel1() {
             new BackgroundObject('img/5_background/layers/3_third_layer/2.png', 719,),
             new BackgroundObject('img/5_background/layers/2_second_layer/2.png', 719),
             new BackgroundObject('img/5_background/layers/1_first_layer/2.png', 719),
-
             new BackgroundObject('img/5_background/layers/air.png', 719 * 2),
             new BackgroundObject('img/5_background/layers/3_third_layer/1.png', 719 * 2),
             new BackgroundObject('img/5_background/layers/2_second_layer/1.png', 719 * 2),
@@ -67,34 +65,26 @@ function initLevel1() {
             new BackgroundObject('img/5_background/layers/3_third_layer/2.png', 719 * 3),
             new BackgroundObject('img/5_background/layers/2_second_layer/2.png', 719 * 3),
             new BackgroundObject('img/5_background/layers/1_first_layer/2.png', 719 * 3),
-
             new BackgroundObject('img/5_background/layers/air.png', 719 * 4),
             new BackgroundObject('img/5_background/layers/3_third_layer/1.png', 719 * 4),
             new BackgroundObject('img/5_background/layers/2_second_layer/1.png', 719 * 4),
             new BackgroundObject('img/5_background/layers/1_first_layer/1.png', 719 * 4),
-
             new BackgroundObject('img/5_background/layers/air.png', 719 * 5),
             new BackgroundObject('img/5_background/layers/3_third_layer/2.png', 719 * 5),
             new BackgroundObject('img/5_background/layers/2_second_layer/2.png', 719 * 5),
             new BackgroundObject('img/5_background/layers/1_first_layer/2.png', 719 * 5),
-
             new BackgroundObject('img/5_background/layers/air.png', 719 * 6),
             new BackgroundObject('img/5_background/layers/3_third_layer/1.png', 719 * 6),
             new BackgroundObject('img/5_background/layers/2_second_layer/1.png', 719 * 6),
             new BackgroundObject('img/5_background/layers/1_first_layer/1.png', 719 * 6),
-
             new BackgroundObject('img/5_background/layers/air.png', 719 * 7),
             new BackgroundObject('img/5_background/layers/3_third_layer/2.png', 719 * 7),
             new BackgroundObject('img/5_background/layers/2_second_layer/2.png', 719 * 7),
             new BackgroundObject('img/5_background/layers/1_first_layer/2.png', 719 * 7),
-
             new BackgroundObject('img/5_background/layers/air.png', 719 * 8),
             new BackgroundObject('img/5_background/layers/3_third_layer/1.png', 719 * 8),
             new BackgroundObject('img/5_background/layers/2_second_layer/1.png', 719 * 8),
             new BackgroundObject('img/5_background/layers/1_first_layer/1.png', 719 * 8),
-
-
-
         ],
 
         [
@@ -112,6 +102,5 @@ function initLevel1() {
         [
 
         ],
-
     );
 }
